c5419699316f0f9007e55e704b6aff20f411f592
Chris Lawrence <chris@sect0r.co.uk>
2018-04-30 08:35:28 -0300

    Updated if statements to use curly braces

--- Src/Clawrenceks.HttpCachingHandler/Extensions/HttpRequestMessageExtensions.cs
+++ Src/Clawrenceks.HttpCachingHandler/Extensions/HttpRequestMessageExtensions.cs
@@ -7,10 +7,14 @@ namespace Clawrenceks.HttpCachingHandler.Extensions
         public static bool ShouldBypassPrivateCache(this HttpRequestMessage request)
         {
             if (request?.Headers?.CacheControl?.NoCache == true)
-            return true;
+            {
+                return true;
+            }
 
             if (request?.Method != HttpMethod.Get)
+            {
                 return true;
+            }
 
             return false;
         }
