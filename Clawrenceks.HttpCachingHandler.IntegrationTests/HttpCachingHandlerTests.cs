9e6be9987b69c4ff47b8731b75d8435253ee792a
Chris Lawrence <chris@sect0r.co.uk>
2018-10-23 18:19:44 -0300

    Final integration test tidy ups

--- Clawrenceks.HttpCachingHandler.IntegrationTests/HttpCachingHandlerTests.cs
+++ Clawrenceks.HttpCachingHandler.IntegrationTests/HttpCachingHandlerTests.cs
@@ -217,46 +217,5 @@ namespace Clawrenceks.HttpCachingHandler.IntegrationTests
                 Assert.Equal(0, expectedStream.Position);
             }
         }
-
-        [Fact]
-        public async Task temp()
-        {
-            //Arrange
-            var httpClient = new HttpClient(new HttpCachingHandler(_fixture.ResponseCache));
-            var requestMessage1 = new HttpRequestMessage(HttpMethod.Get, "https://vignette.wikia.nocookie.net/googology/images/f/f3/Test.jpeg/revision/latest?cb=20180121032443");
-            requestMessage1.Headers.CacheControl = new CacheControlHeaderValue();
-
-            var requestMessage2 = new HttpRequestMessage(HttpMethod.Get, "https://vignette.wikia.nocookie.net/googology/images/f/f3/Test.jpeg/revision/latest?cb=20180121032443");
-            requestMessage1.Headers.CacheControl = new CacheControlHeaderValue();
-
-            //Act
-            var result1 = await httpClient.SendAsync(requestMessage1);
-            var result1Content = await result1.Content.ReadAsStreamAsync();
-
-            var result2 = await httpClient.SendAsync(requestMessage2);
-            var result2Content = await result2.Content.ReadAsStreamAsync();
-
-            //Assert
-            var applicationDirectory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
-            var expectedStream = File.OpenRead(Path.Combine(applicationDirectory, "TestData", "Test.jpg"));
-
-            var fileStream1 = File.Create("C:\\Projects\\Repos\\Clawrenceks.HttpCachingHandler\\Clawrenceks.HttpCachingHandler.IntegrationTests\\bin\\Debug\\netcoreapp2.1\\TestData\\f1.jpg");
-            expectedStream.Seek(0, SeekOrigin.Begin);
-            expectedStream.CopyTo(fileStream1);
-            fileStream1.Close();
-
-            var fileStream2 = File.Create("C:\\Projects\\Repos\\Clawrenceks.HttpCachingHandler\\Clawrenceks.HttpCachingHandler.IntegrationTests\\bin\\Debug\\netcoreapp2.1\\TestData\\f2.jpg");
-            result1Content.Seek(0, SeekOrigin.Begin);
-            result1Content.CopyTo(fileStream2);
-            fileStream2.Close();
-
-            var fileStream3 = File.Create("C:\\Projects\\Repos\\Clawrenceks.HttpCachingHandler\\Clawrenceks.HttpCachingHandler.IntegrationTests\\bin\\Debug\\netcoreapp2.1\\TestData\\f3.jpg");
-            result2Content.Seek(0, SeekOrigin.Begin);
-            result2Content.CopyTo(fileStream3);
-            fileStream3.Close();
-
-            Assert.Equal(expectedStream.Length, result1Content.Length);
-            expectedStream.Close();
-        }
     }
 }
